eae79bff632398b60ce1d3b3ebe509db0cdcd8cd
KJ <info.j.landwoner.k@gmail.com>
2020-05-13 02:33:17 +0900

    refactor

--- commands.go
+++ commands.go
@@ -3,6 +3,7 @@ package issh
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -14,9 +15,7 @@ func ChangeDirectory(tgtdir string) *Command {
 		time.Sleep(time.Second)
 		// fmt.Println(c.Result.Output[c.Result.Lines-2])
 		if c.Result.Output[c.Result.Lines-2] != tgtdir {
-			msg := fmt.Sprintf(
-				"wrong output in cd expect %v got %v", tgtdir, c.Result.Output[c.Result.Lines-2])
-			return false, errors.New(msg)
+			return false, fmt.Errorf("wrong output in cd expect %v got %v", tgtdir, c.Result.Output[c.Result.Lines-2])
 		}
 		return true, nil
 	}
@@ -31,13 +30,15 @@ func SwitchUser(user, password string, newUserPrompt Prompt) *Command {
 
 	callback := func(c *Command) (bool, error) {
 		time.Sleep(time.Second)
-		expect := "パスワード:" //TODO support "Password:" or other prompt pattern
-		if c.Result.Output[c.Result.Lines-1] != expect {
-			msg := fmt.Sprintf(
-				"wrong output in su expect %v got %v", expect, c.Result.Output[c.Result.Lines-1])
-			return false, errors.New(msg)
+		expects := []string{"パスワード:", "Password:"} //TODO support "Password:" or other prompt pattern
+		got := c.Result.Output[c.Result.Lines-1]
+
+		for _, expect := range expects {
+			if strings.TrimRight(got, " ") == expect {
+				return true, nil
+			}
 		}
-		return true, nil
+		return false, fmt.Errorf("wrong output in su expect %v got %v", expects, got)
 	}
 
 	nextCommand := func(c *Command) *Command {
@@ -46,10 +47,8 @@ func SwitchUser(user, password string, newUserPrompt Prompt) *Command {
 			got := c.Result.Output[c.Result.Lines-1]
 			if got[len(got)-newUserPrompt.SufixPosition] != newUserPrompt.SufixPattern {
 				fmt.Println(got)
-				msg := fmt.Sprintf(
-					"wrong output in su expect %v(%v) got %v(%v) RootPassword may be invalid",
+				return false, fmt.Errorf("wrong output in su expect %v(%v) got %v(%v) RootPassword may be invalid",
 					string(newUserPrompt.SufixPattern), newUserPrompt.SufixPattern, string(got[len(got)-2]), got[len(got)-2])
-				return false, errors.New(msg)
 			}
 			return true, nil
 		}
@@ -70,8 +69,7 @@ func CheckUser(expectUser string) *Command {
 		}
 		user := c.Result.Output[c.Result.Lines-3]
 		if user != expectUser {
-			msg := fmt.Sprintf("user is invalid expected %v got %v", expectUser, user)
-			return false, errors.New(msg)
+			return false, fmt.Errorf("user is invalid expected %v got %v", expectUser, user)
 		}
 		return true, nil
 	}
